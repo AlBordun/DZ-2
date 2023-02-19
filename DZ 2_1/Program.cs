77dcbf6fa1a1baea0a6ad8be14fd49e3e1159de8
AleksandrB <albordunw@Gmail.com>
2023-02-19 14:51:45 +0300

    DZ 2 Finale

--- DZ 2_1/Program.cs
+++ DZ 2_1/Program.cs
@@ -3,7 +3,7 @@
 456 -> 5
 782 -> 8
 918 -> 1
-*/
+
 
 Console.Write("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
@@ -11,6 +11,21 @@ int Length = number.ToString().Length;
 if (Length == 3) {
 	int result = ( number/10 ) % 10;
 Console.WriteLine(result);
-} else {
+} 
+else 
+{
+Console.WriteLine("не трехзначное число");
+}
+*/
+
+Console.Write("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99) 
+{
+number = ( number/10 ) % 10;
+Console.WriteLine(number);
+}
+else 
+{
 Console.WriteLine("не трехзначное число");
 }
--- DZ 2_2/Program.cs
+++ DZ 2_2/Program.cs
@@ -3,20 +3,34 @@
 645 -> 5
 78 -> третьей цифры нет
 32679 -> 6
-*/
+
 
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int Length = number.ToString().Length;
 
-if (Length == 3){
+if (Length >= 3){
  while (number > 999)
   number = (number /= 10);
 int result = number % 10;
 System.Console.WriteLine(result);
 
 } else
+{
+  Console.WriteLine("Третьего числа нет");
+}
+*/
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99) {
+while (number >= 999)
+ number = (number /= 10);
+ number = number % 10;
+System.Console.WriteLine(number);
+}
+else
 {
   Console.WriteLine("Третьего числа нет");
 }
